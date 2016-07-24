9ab3ba2f8357f8c88ac5c0bdaead0d38204ef05f
qq244663159 <244663159@qq.com>
2016-07-24 17:01:58 +0800

    click

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 
 class Nav extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      show : true
+    }
+  }
+  handleToggle(){
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
   getStyles(){
+    let show = this.state.show ? "-100%" : "0";
     return{
       navStyle:{
         width:'50px',
@@ -15,13 +28,50 @@ class Nav extends React.Component{
         top:'30px',
         left:'30px',
         color:'black'
+      },
+      ulStyle:{
+        width:'30%',
+        height:'100%',
+        position:'absolute',
+        top:'0',
+        left:'0',
+        padding:'0',
+
+      },
+      liStyle:{
+        listStyle:'none',
+        lineHeight:'50px',
+
+        width:'30%',
+        height:'50px',
+        backgroundColor:'yellow',
+
+
+      },
+      divStyle:{
+        width:show,
+        position:'absolute',
+        top:'0',
+        left:'0',
+        backgroundColor:'#ccc',
+        opacity:'0.5',
+
       }
     }
   }
   render(){
     let style = this.getStyles();
     return(
-      <button style={style.navStyle}>导航</button>
+      <div>
+      <button style={style.navStyle} onClick={this.handleToggle.bind(this)}>导航</button>
+        <div style={style.divStyle} onClick={this.handleToggle.bind(this)}>
+          <ul>
+            <li style={style.liStyle}>first</li>
+            <li style={style.liStyle}>secound</li>
+            <li style={style.liStyle}>third</li>
+          </ul>
+        </div>
+      </div>
     )
   }
 }
